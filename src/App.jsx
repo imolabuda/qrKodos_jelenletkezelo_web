21ce1548b5bdaa741a307c819fd3246c787dbbec
Buda Imola <imola.buda@webapix.hu>
2023-03-18 15:08:18 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  //state - allapot (count), ezt lehet valtoztatni a set fuggvennyel
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
